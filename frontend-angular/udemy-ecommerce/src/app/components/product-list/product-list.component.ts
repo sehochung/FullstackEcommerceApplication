5d3f92eca52054a5ae4c1c1f4d31eeb35ee3d9b4
seho <schung68@wisc.edu>
2022-03-15 11:05:33 -0400

    Added scripts, current frontent and backend application

---
+++ frontend-angular/udemy-ecommerce/src/app/components/product-list/product-list.component.ts
@@ -0,0 +1,123 @@
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { CartItem } from 'src/app/common/cart-item';
+import { Product } from 'src/app/common/product';
+import { CartService } from 'src/app/services/cart.service';
+import { ProductService } from 'src/app/services/product.service';
+
+@Component({
+  selector: 'app-product-list',
+  templateUrl: './product-list-grid.component.html',
+  styleUrls: ['./product-list.component.css']
+})
+export class ProductListComponent implements OnInit {
+
+  products: Product[];
+  currentCategoryId: number = 1 ;
+  previousCategoryId: number = 1;
+  searchMode: boolean = false;
+
+  /// new properties for pagination
+  thePageNumber: number = 1;
+  thePageSize: number = 5;
+  theTotalElements: number = 0;
+
+  previousKeyword: string = null;
+
+  constructor(private productService: ProductService,
+              private route: ActivatedRoute,
+              private cartService: CartService) { }
+
+  ngOnInit(): void {
+    this.route.paramMap.subscribe(() => {
+
+      this.listProducts();
+  
+    });
+  }
+  listProducts() {
+
+    // check if "id" parameter is available
+    this.searchMode = this.route.snapshot.paramMap.has('keyword');
+    if (this.searchMode) {
+      this.handleSearchProducts();
+    }
+    else {
+      this.handleListProducts();
+
+    }
+    
+  }
+  handleSearchProducts() {
+    const theKeyword: string = this.route.snapshot.paramMap.get('keyword');
+
+    // if we have a different keyword than previous
+    // set thePageNumber to 1
+    if (this.previousKeyword != theKeyword) {
+      this.thePageNumber = 1;
+    }
+
+    this.previousKeyword = theKeyword;
+
+    console.log(`keyword=${theKeyword}, thePageNumber=${this.thePageNumber}`);
+
+    // now search for the products using keyword
+    this.productService.searchProductsPaginate(this.thePageNumber-1,
+                      this.thePageSize,
+                      theKeyword).subscribe(this.processResult());
+  }
+   
+  handleListProducts() {
+    const hasCategoryId: boolean = this.route.snapshot.paramMap.has('id');
+
+    if (hasCategoryId) {
+      this.currentCategoryId = +this.route.snapshot.paramMap.get('id');
+    }
+    else {
+      this.currentCategoryId = 1;
+    }
+
+    // check category id from now and previous
+    // note: angular will reuse a component if being viewed
+    if (this.previousCategoryId != this.currentCategoryId) {
+      this.thePageNumber = 1;
+    }
+    
+    this.previousCategoryId = this.currentCategoryId;
+    console.log(`currentCategoryId=${this.currentCategoryId}, thePageNumber=${this.thePageNumber}`);
+
+
+    this.productService.getProductListPaginate(this.thePageNumber-1, 
+                                                this.thePageSize, 
+                                                this.currentCategoryId)
+                                                .subscribe(this.processResult());
+  }
+  processResult() {
+    
+    return data => {
+      this.products = data._embedded.products;
+      this.thePageNumber = data.page.number + 1;
+      this.thePageSize = data.page.size;
+      this.theTotalElements = data.page.totalElements;
+      
+    };
+  }
+
+  updatePageSize(pageSize: number) {
+    this.thePageSize = pageSize;
+    this.thePageNumber = 1;
+    this.listProducts();
+
+  }
+
+  addToCart(theProduct: Product) {
+    console.log(`Adding to cart: ${theProduct.name}, ${theProduct.unitPrice}`);
+
+    const theCartItem = new CartItem(theProduct);
+
+    this.cartService.addToCart(theCartItem);
+
+  
+  }
+
+}
---
+++ frontend-angular/udemy-ecommerce/src/app/config/my-app-config.ts
@@ -0,0 +1,11 @@
+export default {
+    
+    oidc: {
+        clientId: '0oa44r0b7gfJKncFx5d7',
+        issuer: 'https://dev-9172097.okta.com/oauth2/default',
+        redirectUri: 'http://localhost:4200/login/callback',
+        scopes: ['openid', 'profile', 'email']
+    }
+
+
+}
