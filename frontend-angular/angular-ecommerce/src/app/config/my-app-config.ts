f650a9a3e65f6d94876527c307c385f901a57cca
Anna Archut <anna.archut@hcl.com>
2022-03-16 13:41:02 -0400

    new front-end

---
+++ frontend-angular/angular-ecommerce/src/app/config/my-app-config.ts
@@ -0,0 +1,10 @@
+export default {
+
+    oidc: {
+        clientId: '0oa46q823pISLnySn5d7',
+        issuer: 'https://dev-4328024.okta.com/oauth2/default',
+        redirectUri: 'http://localhost:4200/login/callback',
+        scopes: ['openid', 'profile', 'email']
+    }
+
+}
